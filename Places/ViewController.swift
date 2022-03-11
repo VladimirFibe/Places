56bd0c32ff7cc245a9570cae691a3173f4d0f9a7
Vladimir Fibe <motiw@icloud.com>
2022-03-11 17:14:27 +0600

    Initial Commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Vladimir Fibe on 11.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
